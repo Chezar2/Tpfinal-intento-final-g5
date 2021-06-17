f83cc81639a1f4be8bafc5a9571d70066002c68c
solis <solis@DESKTOP-LF50LIN>
2021-06-17 12:35:54 -0300

    la inicializacion del proyecto

---
+++ Tp-final-grupo5-v2/src/main/java/com/example/demo/TpFinalGrupo5V2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalGrupo5V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalGrupo5V2Application.class, args);
+	}
+
+}
---
+++ Tp-final-grupo5-v2/src/test/java/com/example/demo/TpFinalGrupo5V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalGrupo5V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
